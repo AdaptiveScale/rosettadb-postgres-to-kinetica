b9644ea461f0cf739361558dd8788f313eb84b57
nuhibesimi <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>
2024-07-05 12:20:08 +0200

    postgres to kinetica using rosettadb

--- scripts/rosetta_test.sh
+++ scripts/rosetta_test.sh
@@ -10,8 +10,8 @@ echo "#                   #"
 echo "#####################"
 
 export ROSETTA_DRIVERS=/home/runner/work/rosetta-ci-cd/rosetta-ci-cd/drivers/*
-export PATH=$PATH:/home/runner/work/rosetta-ci-cd/rosetta-ci-cd/rosetta/bin/
-export PATH=$PATH:/home/runner/work/rosetta-ci-cd/rosetta-ci-cd/kinetica/
+export PATH=$PATH:/home/runner/work/rosettadb-postgres-to-kinetica/rosettadb-postgres-to-kinetica/rosetta/bin/
+export PATH=$PATH:/home/runner/work/rosettadb-postgres-to-kinetica/rosettadb-postgres-to-kinetica/kinetica/
 
 echo "[DEBUG] ROSETTA_DRIVERS = $ROSETTA_DRIVERS"
 echo "[DEBUG] PATH=$PATH"
