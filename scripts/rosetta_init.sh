7def64c9e7546120720696f4ac9512021dc6bc87
nuhibesimi <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>
2024-07-05 12:15:23 +0200

    postgres to kinetica using rosettadb

--- scripts/rosetta_init.sh
+++ scripts/rosetta_init.sh
@@ -9,17 +9,14 @@ echo "#  Starting Script  #"
 echo "#                   #"
 echo "#####################"
 
-export BIGQUERY_JDBC_DRIVER=drivers/*
 export OPENAI_API_KEY=$OPENAI_API_KEY
 
-export ROSETTA_DRIVERS=/home/runner/work/rosetta-ci-cd/rosetta-ci-cd/drivers/*
-export PATH=$PATH:/home/runner/work/rosetta-ci-cd/rosetta-ci-cd/rosetta/bin/
-export PATH=$PATH:/home/runner/work/rosetta-ci-cd/rosetta-ci-cd/kinetica/
+export ROSETTA_DRIVERS=/home/runner/work/rosettadb-postgres-to-kinetica/rosettadb-postgres-to-kinetica/drivers/*
+export PATH=$PATH:/home/runner/work/rosettadb-postgres-to-kinetica/rosettadb-postgres-to-kinetica/rosetta/bin/
+export PATH=$PATH:/home/runner/work/rosettadb-postgres-to-kinetica/rosettadb-postgres-to-kinetica/kinetica/
 
 echo "[DEBUG] ROSETTA_DRIVERS=$ROSETTA_DRIVERS"
 echo "[DEBUG] PATH=$PATH"
-
-export GOOGLE_CREDS_PATH=/home/runner/work/rosetta-ci-cd/rosetta-ci-cd/service-account.json
 
 echo "[DEBUG] Show rosetta version"
 rosetta --version
