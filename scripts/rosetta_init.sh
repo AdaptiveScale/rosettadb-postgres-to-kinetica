a37e7eb6cbcb919c9c923a6ef955c2138ffee822
nuhibesimi <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>
2024-07-09 09:43:25 +0200

    postgres to kinetica

--- scripts/rosetta_init.sh
+++ scripts/rosetta_init.sh
@@ -14,9 +14,11 @@ export OPENAI_API_KEY=$OPENAI_API_KEY
 export ROSETTA_DRIVERS=/home/runner/work/rosettadb-postgres-to-kinetica/rosettadb-postgres-to-kinetica/drivers/*
 export PATH=$PATH:/home/runner/work/rosettadb-postgres-to-kinetica/rosettadb-postgres-to-kinetica/rosetta/bin/
 export PATH=$PATH:/home/runner/work/rosettadb-postgres-to-kinetica/rosettadb-postgres-to-kinetica/kinetica/
+export EXTERNAL_TRANSLATION_FILE=/home/runner/work/rosettadb-postgres-to-kinetica/rosettadb-postgres-to-kinetica/translation/translation.csv
 
 echo "[DEBUG] ROSETTA_DRIVERS=$ROSETTA_DRIVERS"
 echo "[DEBUG] PATH=$PATH"
+echo "[DEBUG] EXTERNAL_TRANSLATION_FILE=$EXTERNAL_TRANSLATION_FILE"
 
 echo "[DEBUG] Show rosetta version"
 rosetta --version
